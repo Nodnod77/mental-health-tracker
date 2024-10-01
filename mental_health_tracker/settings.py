dd1b31deedbd90dcc8e4d1368fc3ddd071700cfc
Nodnod77 <donia.sakji@etu.imt-nord-europe.fr>
2024-10-01 14:41:28 +0700

    zz

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -130,7 +130,7 @@ else:
 
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field !
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field !!
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
